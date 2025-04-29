8e2eb8e43d755b88c7546682a2a4458d4ee75ea2
huhen <kinetic@bk.ru>
2025-04-29 21:13:10 +0300

    Updated draft.cpp

--- sprint1/draft.cpp
+++ sprint1/draft.cpp
@@ -1,6 +1,54 @@
 #include <cstddef>
 #include <memory>
 
+class Image {};
+
+class Box {
+public:
+    Box()
+        : imageData_(new Image{}) {
+    }
+
+    // Ваш код здесь
+    ~Box() {
+        // Cleanup code here
+        delete imageData_;
+    }
+
+    Box(const Box& other)
+        : imageData_(new Image(*other.imageData_)) {
+        // Copy code here
+    }
+
+    Box(Box&& other) noexcept
+        : imageData_(other.imageData_) {
+        // Move code here
+        other.imageData_ = nullptr;
+    }
+
+    Box& operator=(const Box& other) {
+        // Copy assignment code here
+        if (this != &other) {
+            delete imageData_;
+            imageData_ = new Image(*other.imageData_);
+        }
+        return *this;
+    }
+
+    Box& operator=(Box&& other) noexcept {
+        // Move assignment code here
+        if (this != &other) {
+            delete imageData_;
+            imageData_ = other.imageData_;
+            other.imageData_ = nullptr;
+        }
+        return *this;
+    }
+
+private:
+    Image* imageData_;
+};
+
 static void uptr() {
     const std::size_t size = 10;
     auto pArr = std::make_unique<int[]>(size);

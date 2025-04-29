35b2bf894076a8e91aeb754a12788955a5c8491a
huhen <kinetic@bk.ru>
2025-04-29 22:56:07 +0300

    update

--- sprint1/draft.cpp
+++ sprint1/draft.cpp
@@ -1,6 +1,74 @@
 #include <cstddef>
 #include <memory>
 
+#include <cstring>
+#include <utility>
+
+class Matrix {
+public:
+    Matrix(size_t rows, size_t cols)
+        : rows_(rows)
+        , cols_(cols)
+        , data_(new int[rows * cols]{}) {
+    }
+
+    ~Matrix() {
+        delete[] data_;
+    }
+
+    Matrix(const Matrix& rhd)
+        : rows_(rhd.rows_)
+        , cols_(rhd.cols_)
+        , data_(new int[rhd.rows_ * rhd.cols_]) {
+        std::memcpy(data_, rhd.data_, rows_ * cols_ * sizeof(int));
+    }
+
+    Matrix& operator=(const Matrix& rhd) {
+        if (this != &rhd) {
+            delete[] data_;
+
+            rows_ = rhd.rows_;
+            cols_ = rhd.cols_;
+
+            data_ = new int[rhd.rows_ * rhd.cols_];
+            std::memcpy(data_, rhd.data_, rows_ * cols_ * sizeof(int));
+        }
+        return *this;
+    }
+
+    // Ваш код здесь
+    Matrix(Matrix&& rhd) noexcept
+        : rows_(std::exchange(rhd.rows_, 0))
+        , cols_(std::exchange(rhd.cols_, 0))
+        , data_(std::exchange(rhd.data_, nullptr)) {
+    }
+
+    Matrix& operator=(Matrix&& rhd) noexcept {
+        if (this != &rhd) {
+            rows_ = std::exchange(rhd.rows_, 0);
+            cols_ = std::exchange(rhd.cols_, 0);
+            delete[] data_;
+            data_ = std::exchange(rhd.data_, nullptr);
+        }
+        return *this;
+    }
+
+    void swap(Matrix& other) noexcept {
+        std::swap(data_, other.data_);
+        std::swap(rows_, other.rows_);
+        std::swap(cols_, other.cols_);
+    }
+
+    friend void swap(Matrix& lhs, Matrix& rhs) noexcept {
+        lhs.swap(rhs);
+    }
+
+private:
+    size_t rows_;
+    size_t cols_;
+    int* data_;
+};
+
 class Image {};
 
 class Box {

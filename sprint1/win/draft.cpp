2eec5d980a172f23dcb0f628fdccbf5cc405e37e
Ivan <ivan@autoscop.ru>
2025-04-30 02:50:45 +0300

    final tune win

--- sprint1/win/draft.cpp
+++ sprint1/win/draft.cpp
@@ -1,9 +1,24 @@
+#include <windows.h>
 #include <print>
+#include <processthreadsapi.h>
+#include <handleapi.h>
+#include <wil/resource.h>
 
+void test() {
+    wil::unique_process_handle hProcess(OpenProcess(PROCESS_ALL_ACCESS, FALSE, GetCurrentProcessId()));
+
+    // HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, GetCurrentProcessId());
+    // if (hProcess) {
+    //     CloseHandle(hProcess);
+    // }
+}
 
 int main() {
-
-    std::print("Hello!");
+    std::print("Hello!\n");
+    test();
+    std::print("Exit\n");
 
     return 0;
 }
+
+

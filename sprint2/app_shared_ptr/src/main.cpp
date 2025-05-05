bbc02eba6fc702d34296b455c3b6e7b54170637b
huhen <kinetic@bk.ru>
2025-05-05 11:10:46 +0300

    app_shared_ptr

--- sprint2/app_shared_ptr/src/main.cpp
+++ sprint2/app_shared_ptr/src/main.cpp
@@ -3,15 +3,18 @@
 #include <string>
 #include <vector>
 
-class Widget : public std::enable_shared_from_this<Widget> {
-
-    Widget(std::string_view name, std::shared_ptr<Widget> parent) : name(name_), parent(parent_) {}
+class Widget {
+public:
+    Widget(std::string_view name, Widget* parent)
+        : name_(name)
+        , parent_(parent) {
+    }
 
     // Метод для создания дочернего виджета
-    std::shared_ptr<Widget> CreateChild(std::string_view childName) {
-        auto child = std::make_shared<Widget>(childName, shared_from_this());
-        children_.push_back(child);
-        return child;
+    Widget* CreateChild(std::string_view childName) {
+        auto child = std::make_unique<Widget>(childName, this);
+        children_.push_back(std::move(child));
+        return children_.back().get();
     }
 
     // Метод для отображения иерархии виджетов
@@ -24,19 +27,19 @@ class Widget : public std::enable_shared_from_this<Widget> {
             std::print("{} - {}", indent, name_);
         }
 
-        for (const auto &child : children_) {
+        for (const auto& child : children_) {
             child->PrintHierarchy(level + 1);
         }
     }
 
 private:
     std::string name_;
-    std::weak_ptr<Widget> parent_;                   // Слабая ссылка на родителя
-    std::vector<std::shared_ptr<Widget>> children_;  // Список дочерних виджетов
+    Widget* parent_;                                // Слабая ссылка на родителя
+    std::vector<std::unique_ptr<Widget>> children_; // Список дочерних виджетов
 };
 
 int main() {
-    auto root = std::make_shared<Widget>("Root");  // Главное окно
+    auto root = std::make_unique<Widget>("Root", nullptr); // Главное окно
 
     // Создание дочерних виджетов
     auto child1 = root->CreateChild("Child1");

7c6fa4190a0b677b8de393a071be3e35be47a130
huhen <kinetic@bk.ru>
2025-05-05 15:27:03 +0300

    app_dyn_cfg

--- sprint2/draft.cpp
+++ sprint2/draft.cpp
@@ -1,5 +1,19 @@
-#include <cstddef>
+#include <iostream>
+#include <thread>
+#include <chrono>
 
 int main() {
-    return 0;
-}
+    std::jthread worker([](std::stop_token st) {
+        for (int i = 0; i < 5; ++i) {
+            if (st.stop_requested()) {
+                std::cout << "Остановка потока\n";
+                return;
+            }
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::cout << "Работаю... " << i << "\n";
+        }
+    });
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    worker.request_stop(); // Запрос остановки
+} // Деструктор worker автоматически вызывает join()
